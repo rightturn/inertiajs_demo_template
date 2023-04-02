60b422c00778d31c4c574a8da5589aee221749ef
Amna Sherazi <amnasherazi94@gmail.com>
2023-04-02 18:18:21 +0500

    Two routes added

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,10 @@ Route::get('/', function () {
     return Inertia::render('Test');
 });
 
-// Route::get('/users', function () {
-//     return Inertia::render('Users');
-// });
+Route::get('/users', function () {
+    return Inertia::render('Users');
+});
 
-// Route::get('/settings', function () {
-//     return Inertia::render('Settings');
-// });
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
+});
